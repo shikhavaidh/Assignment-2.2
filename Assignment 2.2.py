c76304b05bca70172953ffb549cc1458659639a3
shikhavaidh <36124149+shikhavaidh@users.noreply.github.com>
2018-02-13 16:05:16 +0530

    Assignment 2.2
    Assignment 2.2

---
+++ Assignment 2.2.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[49]:
+
+for i in range(0, 5):
+    for j in range(0, i+1):
+        print("* ",end="")
+    print()
+for i in range(0, 5):
+    for j in range(4, i, -1):
+        print("* ", end="")
+    print()
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
